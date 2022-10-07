46a97a14548ba3aea981ae1c9a98aaba73f2b99e
zhaosuizhi <zhaosuizhi@duck.com>
2022-10-07 17:15:32 +0800

    :construction: 实现WebDriver工厂类

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 from aum.config import ConfigFactory
+from aum.driver import WebDriverFactory
 
 
 def main():
     config = ConfigFactory().create()
+
+    driver = WebDriverFactory(hub_url=config.sel_hub, headless=False).create()
+    driver.get(config.unlock_music_server)
 
 
 if __name__ == '__main__':
